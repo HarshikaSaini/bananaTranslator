a17ae61726eb7937fd5c7e72a67d6970d9676994
Harshika <harshikasaini1415@gmail.com>
2023-04-22 00:04:39 +0530

    feat: added event feature

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('hello world');
+var btnclick = document.querySelector("#btn_Click");
+var inputText = document.querySelector("#innerText");
+
+btnclick.addEventListener("click", function handleEvent() {
+    console.log("input", inputText.value);
+
+});
+
+
+
