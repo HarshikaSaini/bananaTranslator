21493bb4b341e6feb084093932f7e195cc648709
Harshika <harshikasaini1415@gmail.com>
2023-04-22 16:48:13 +0530

    feat: connecting minion's translate api

--- app.js
+++ app.js
@@ -2,12 +2,28 @@ var btnclick = document.querySelector("#btn_Click");
 var input = document.querySelector("#inputText");
 var output=document.querySelector("#outputText");
 
+var url="https://api.funtranslations.com/translate/minion.json"
+function urlHandler(text){
+   return url + "?" + "text=" + text;
+} 
 
-btnclick.addEventListener("click", function handleEvent() {
-    // console.log("input", input.value);
-    console.log(output.innerText = "hello " + input.value);
+function errorHandler(error){
+    console.log("Error occured" , error)
+}
 
-}); 
+function handleEvent(){
+    var finalInput = input.value;
+    fetch(urlHandler(finalInput))
+        .then(response => response.json())
+        .then(json =>  {
+            output.innerText=json.contents.translated;
+        })
+        .catch(errorHandler)
+}
+
+
+btnclick.addEventListener("click", handleEvent);
+   
 
 
 
